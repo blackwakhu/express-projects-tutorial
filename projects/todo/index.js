f2b05efd75402071b068e8862cbc31578124780d
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-31 13:21:35 +0300

    added the code to get data from a form and another for changing the view engine

--- projects/todo/index.js
+++ projects/todo/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
 
 const app = express();
 
+let data = ["go to class"]
+
+// this is for setting the body parser for getting data from a form
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// this is for setting the template
 app.set('views',path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) =>{
   res.render('index')
+})
+
+app.get('/todo', (req, res)=>{
+  data.forEach((val)=>{console.log(val)})
+  res.render('todo', {"data": data})
+})
+
+app.post('/todo', (req, res)=>{
+  data.push(req.body.item)
+  res.redirect('/todo')
 })
 
 app.listen(3000, () =>{
